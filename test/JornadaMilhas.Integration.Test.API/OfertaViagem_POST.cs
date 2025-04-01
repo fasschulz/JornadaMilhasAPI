d99d4768cd5a0be941122c745adef99e47b75baa
Fernando Schulz <fasschulz@gmail.com>
2025-04-01 15:18:31 -0300

    Aplicando conceito de class fixture

--- test/JornadaMilhas.Integration.Test.API/OfertaViagem_POST.cs
+++ test/JornadaMilhas.Integration.Test.API/OfertaViagem_POST.cs
@@ -10,13 +10,18 @@ using System.Threading.Tasks;
 
 namespace JornadaMilhas.Integration.Test.API;
 
-public class OfertaViagem_POST
+public class OfertaViagem_POST : IClassFixture<JornadaMilhasWebApplicationFactory>
 {
+    private readonly JornadaMilhasWebApplicationFactory app;
+
+    public OfertaViagem_POST(JornadaMilhasWebApplicationFactory app)
+    {
+        this.app = app;
+    }
+
     [Fact]
     public async Task Cadastra_Oferta_Viagem()
-    {        
-        var app = new JornadaMilhasWebApplicationFactory();
-
+    {      
         using var client = await app.GetClientWithAccessTokenAsync();
 
         var ofertaViagem = new OfertaViagem()
@@ -29,5 +34,22 @@ public class OfertaViagem_POST
         var response = await client.PostAsJsonAsync("/ofertas-viagem", ofertaViagem);
                 
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task Cadastra_Oferta_Viagem_SemAutorizacao()
+    {        
+        using var client = app.CreateClient();
+
+        var ofertaViagem = new OfertaViagem()
+        {
+            Preco = 100,
+            Rota = new Rota("Origem", "Destino"),
+            Periodo = new Periodo(DateTime.Parse("2024-03-03"), DateTime.Parse("2024-03-06"))
+        };
+
+        var response = await client.PostAsJsonAsync("/ofertas-viagem", ofertaViagem);
+
+        Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
     }
 }
