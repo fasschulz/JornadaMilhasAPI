054db9db9ca7f477a6a1da8c63b2d4b586aba6a5
Fernando Schulz <fasschulz@gmail.com>
2025-04-01 15:09:06 -0300

    Teste POST oferta viagem com autenticacao do usuario

--- test/JornadaMilhas.Integration.Test.API/JornadaMilhasWebApplicationFactory.cs
+++ test/JornadaMilhas.Integration.Test.API/JornadaMilhasWebApplicationFactory.cs
@@ -1,9 +1,12 @@
-﻿using JornadaMilhas.Dados;
+﻿using JornadaMilhas.API.DTO.Auth;
+using JornadaMilhas.Dados;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
 
 namespace JornadaMilhas.Integration.Test.API;
 
@@ -21,5 +24,21 @@ public class JornadaMilhasWebApplicationFactory : WebApplicationFactory<Program>
                  ("Server=localhost,11433;Database=JornadaMilhasV3;User Id=sa;Password=Alura#2024;Encrypt=false;TrustServerCertificate=true;MultipleActiveResultSets=true;"));
         });
         base.ConfigureWebHost(builder);
+    }
+
+    public async Task<HttpClient> GetClientWithAccessTokenAsync()
+    {
+        var client = this.CreateClient();
+        var user = new UserDTO { Email = "tester@email.com", Password = "Senha123@" };
+        var resultado = await client.PostAsJsonAsync("/auth-login", user);
+
+        resultado.EnsureSuccessStatusCode();
+
+        var result = await resultado.Content.ReadFromJsonAsync<UserTokenDTO>();
+
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", result!.Token);
+
+        return client;
+
     }
 }
---
+++ test/JornadaMilhas.Integration.Test.API/OfertaViagem_POST.cs
@@ -0,0 +1,33 @@
+﻿using JornadaMilhas.Dominio.Entidades;
+using JornadaMilhas.Dominio.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JornadaMilhas.Integration.Test.API;
+
+public class OfertaViagem_POST
+{
+    [Fact]
+    public async Task Cadastra_Oferta_Viagem()
+    {        
+        var app = new JornadaMilhasWebApplicationFactory();
+
+        using var client = await app.GetClientWithAccessTokenAsync();
+
+        var ofertaViagem = new OfertaViagem()
+        {
+            Preco = 100,
+            Rota = new Rota("Origem", "Destino"),
+            Periodo = new Periodo(DateTime.Parse("2024-03-03"), DateTime.Parse("2024-03-06"))
+        };
+        
+        var response = await client.PostAsJsonAsync("/ofertas-viagem", ofertaViagem);
+                
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+}
