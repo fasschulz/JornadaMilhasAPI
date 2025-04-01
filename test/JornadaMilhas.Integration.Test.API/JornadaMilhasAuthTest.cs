a09d62d5d96075f0590684f638f2ba705f688c45
Fernando Schulz <fasschulz@gmail.com>
2025-04-01 11:53:00 -0300

    Testando endpoint de login

---
+++ test/JornadaMilhas.Integration.Test.API/JornadaMilhasAuthTest.cs
@@ -0,0 +1,22 @@
+using JornadaMilhas.API.DTO.Auth;
+using System.Net;
+using System.Net.Http.Json;
+
+namespace JornadaMilhas.Integration.Test.API
+{
+    public class JornadaMilhasAuthTest
+    {
+        [Fact]
+        public async Task POST_Efetua_Login_Com_Sucesso()
+        {
+            var app = new JornadaMilhasWebApplicationFactory();
+            var user = new UserDTO { Email = "tester@email.com", Password = "Senha123@" };
+
+            using var client = app.CreateClient();
+
+            var resultado = await client.PostAsJsonAsync("/auth-login", user);
+
+            Assert.Equal(HttpStatusCode.OK, resultado.StatusCode);
+        }
+    }
+}
